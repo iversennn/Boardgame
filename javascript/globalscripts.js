f5600494f54021bb66bbf2f00133e17da959117f
Simen Iversen <simen.m.iversen@gmail.com>
2020-04-29 09:06:46 +0200

    Startet adding Javascript

---
+++ javascript/globalscripts.js
@@ -0,0 +1,19 @@
+// Cursor coordinate functions
+/*var myX, myY, xyOn, myMouseX, myMouseY;
+xyOn = true;
+function getXYPosition(e){
+myMouseX=(e||event).clientX-235;
+myMouseY=(e||event).clientY-70;
+if (document.documentElement.scrollTop > 0) {
+myMouseY = myMouseY + document.documentElement.scrollTop;
+}
+if (xyOn) {
+console.log("X is " + myMouseX + "\nY is " + myMouseY);
+}
+}
+function toggleXY() {
+xyOn = !xyOn;
+document.getElementById('xyLink').blur();
+return false;
+}
+document.onmouseup=getXYPosition;*/

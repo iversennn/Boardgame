08eae9e5b17019c47ff2e26fffdd4911cacbcc4e
Simen Iversen <simen.m.iversen@gmail.com>
2020-05-05 16:23:19 +0200

    changed audio to fit better

--- javascript/music.js
+++ javascript/music.js
@@ -1,6 +1,8 @@
-var audio = new Audio('./media/music/tavern.mp3');
+var audio = new Audio('./media/music/tavernNew.mp3');
 
 function gameMusic(){
+    audio.volume=0.3;
+    audio.loop=true;
     audio.play();
 }
 

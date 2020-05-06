027bdd2e9fcfef6430ca2ab2d191730b7f125ea1
Simen Iversen <simen.m.iversen@gmail.com>
2020-05-06 13:29:08 +0200

    Started on winner page

--- javascript/winner.js
+++ javascript/winner.js
@@ -1,9 +1,7 @@
 function congratzToWinner(){
     var myText = document.querySelector('.winnerText');
-    console.log(myText.innerHTML);
     if (localStorage.p1 > 28){
     myText.innerHTML = 'Congratulations ' + localStorage.playerOneName + '! You won the game!';
-    console.log(myText);
     } else if (localStorage.p2 >28){
     myText.innerHTML = 'Congratulations ' + localStorage.playerTwoName + '! You won the game!';
     }
